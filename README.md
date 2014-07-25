f202805ebe94e1d682797694ca3c7c75c5f3ed8a
Mikko Leino <mikko@viiksipojat.fi>
2014-07-25 13:41:33 +0300

    init

---
+++ README.md
@@ -0,0 +1,30 @@
+# bash_oops
+
+Tweet your failures! 
+
+## Prerequisites
+
+Install [Bash OAuth](https://github.com/livibetter/bash-oauth), like this `make install-twitter`.
+
+## Installation
+
+1. Source `bash_oopser.sh` in your `.bashrc`
+2. Create `.bash_oops.rc`:
+	cat > $HOME/.bash_oopser.rc <<TEMPLATE
+	oauth_consumer_key=
+	oauth_consumer_secret=
+	TEMPLATE
+3. Fill in your [Twitter API keys](http://dev.twitter.com/apps/new) to the newly created `$HOME/.bash_oops.rc`
+4. Go crazy
+
+## Usage
+
+Make typos.
+
+## Questions
+
+Are there any security concerns? Yes, plenty. A wise man wouldn't do this.
+
+## TODO
+
+There are a bunch of [SMS commands](https://support.twitter.com/articles/14020-twitter-for-sms-basic-features), maybe they should be handled somehow.
---
+++ bash_oopser.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+BASH_OOPS_RC="$HOME/.bash_oops.rc"
+source "$BASH_OOPS_RC" # apikeys & tokens
+
+# bashoauth depends on md5sum, but macosx provides md5
+command -v md5sum >/dev/null 2>&1 || alias md5sum='md5 -r'
+
+source $(which TwitterOAuth.sh)
+TO_init
+
+if [[ "$oauth_token" == "" ]] || [[ "$oauth_token_secret" == "" ]]; then
+	TO_access_token_helper
+	if (( $? == 0 )); then
+		oauth_token=${TO_ret[0]}
+		oauth_token_secret=${TO_ret[1]}
+		echo "oauth_token='${TO_ret[0]}'" >> "$BASH_OOPS_RC"
+		echo "oauth_token_secret='${TO_ret[1]}'" >> "$BASH_OOPS_RC"
+	else
+		echo 'bash_oops failed to get twitter access token'
+	fi
+fi
+
+tweet_ur_failure() {
+	failure=$1
+	[ ${#failure} -gt 140 ] && failure=${failure:0:139}…
+	TO_statuses_update "$failure"
+}
+trap 'tweet_ur_failure "$BASH_COMMAND"' ERR
